93e052f09816b4d30e9cbf5326d90f3bc2fb3d9b
Konrad Rybicki <konradrybicki.altamoda@gmail.com>
2021-11-23 22:48:02 +0100

    feat(main.py): implement 'getMovies' endpoint

--- Restful_API/main.py
+++ Restful_API/main.py
@@ -1,26 +1,36 @@
-# -*- coding: utf-8 -*-
+#%% Movie data gathering
 
 from src.FileReader import FileReader
 from src.Movie import Movie
 
-# source file data read
-
-rows: [[str]] = FileReader.getRows()
-
-# read data modelling and serialization
-
-movies_json = []
-
-for row in rows:
+def getMovieData():
     
-    id = row[0]
-    title = row[1]
-    genres = row[2]
-    
-    movie = Movie(id, title, genres)
-    movies_json.append(movie.__dict__)
-    
-# (print)
+    # source file data read
 
-for movie in movies_json:
-    print(movie)
+    rows: [[str]] = FileReader.getRows()
+    
+    # read data modelling and serialization
+    
+    movies_json = []
+    
+    for row in rows:
+        
+        id = row[0]
+        title = row[1]
+        genres = row[2]
+        
+        movie = Movie(id, title, genres)
+        movies_json.append(movie.__dict__)
+    
+    return movies_json
+    
+#%% Flask app initialization
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/movies")
+def getMovies():
+    movies = getMovieData()
+    return jsonify(movies)
