2a517f0767cbfba8706d23f22aaebd96d6973ad9
Konrad Rybicki <konradrybicki.altamoda@gmail.com>
2021-11-26 00:26:17 +0100

    fix(FileReader): adjust FileReader class to read data from other source files

--- Restful_API/src/FileReader.py
+++ Restful_API/src/FileReader.py
@@ -4,30 +4,16 @@ import csv
 
 class FileReader:
     
-    """ Is responsible for reading data from a source file (movies.csv) """
+    """ Is responsible for reading data from the source files """
     
-    __sourceFilePath = r'C:\Moje pliki\Studia\II stopień\I sem\Programowanie\Laby\Restful_API\movies.csv'
-    
-    @classmethod
-    def getHeader(cls) -> [str]:
-        
-        """ Reads only the header from a source file """
-        
-        f = open(cls.__sourceFilePath)
-        
-        reader = csv.reader(f)
-        header = next(reader)
-        
-        f.close()
-        
-        return header
+    sourceFilePath: str
     
     @classmethod
     def getRows(cls) -> [[str]]:
         
-        """ Reads all the rows from a source file (excluding the header) """
+        """ Reads all the rows from a specified source file (excluding the header) """
         
-        f = open(cls.__sourceFilePath, encoding='utf8')
+        f = open(cls.sourceFilePath, encoding='utf8')
         
         reader = csv.reader(f)
         rows = []
