ec1b0b882e5cf18be23b30cd5eb9d944bdd12f24
Konrad Rybicki <konradrybicki.altamoda@gmail.com>
2021-11-29 15:48:46 +0100

    fix(FileReaderTests.py): change absolute 'movies.csv' file path to one basing on the relative path

--- Restful_API/tests/FileReaderTests.py
+++ Restful_API/tests/FileReaderTests.py
@@ -1,18 +1,44 @@
 # -*- coding: utf-8 -*-
 
+#%% unit tests
+
 import unittest
 from src.FileReader import FileReader
 
 class FileReaderTests(unittest.TestCase):
     
-    def testGetHeader(self):
-        header = FileReader.getHeader()
-        self.assertIsNotNone(header)
-        
-    def testGetRows(self):
-        rows = FileReader.getRows()
-        rowsEmpty = not rows
-        self.assertFalse(rowsEmpty)
+    # boundary conditions - success
     
+    def testGetRows_movies(self):
+        
+        # given
+        movies_relative = '../data/movies.csv'
+        movies_absolute = getAbsolutePathUsing(movies_relative)
+        FileReader.sourceFilePath = movies_absolute
+        
+        # when
+        rows: [[str]] = FileReader.getRows()
+        
+        # then
+        rowsEmpty: bool = not rows
+        self.assertFalse(rowsEmpty)
+        
+    
+
+#%% helper method
+
+import os
+
+def getAbsolutePathUsing(pathRelativeToCurrentFile: str) -> str:
+    
+    """ Returns an absolute path to a certain location, using a path relative to the current file """    
+    
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, pathRelativeToCurrentFile)
+    
+    return filename
+
+#%% tests launch
+
 if __name__ == "__main__":
     unittest.main()
