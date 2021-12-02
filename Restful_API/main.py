9609321fb1bc04f365fc9d7a0e87993b7ec59ed7
Konrad Rybicki <konradrybicki.altamoda@gmail.com>
2021-12-02 01:21:32 +0100

    feat(main.py): implement data gathering methods

--- Restful_API/main.py
+++ Restful_API/main.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+#%% JSON type alias
+
+from typing import NewType
+json = NewType("json", dict)
+
 #%% flask app initialization
 
 from flask import Flask, jsonify
@@ -8,19 +13,20 @@ app = Flask(__name__)
 
 @app.route("/movies")
 def getMovies():
-    movies = getMovieData()
+    movies: [json] = getJsonData_movies()
     return jsonify(movies)
 
-#%% helper methods
+#%% data gathering methods
 
 from src.FileReader import FileReader
-import src.Data
+from src.Data import Movie, Link, Rating, Tag
 
+# (helper method)
 from tests.FileReaderTests import getAbsolutePathUsing
 
-def getMovieData():
+def getJsonData_movies():
     
-    # source file data read
+    # source file data reading
     
     movies_relative = "data/movies.csv"
     movies_absolute = getAbsolutePathUsing(movies_relative)
@@ -32,7 +38,7 @@ def getMovieData():
     
     movies_json = []
     
-    for row in rows:
+    for row in movies:
         
         movieID: int = row[0]
         title: str = row[1]
@@ -42,6 +48,68 @@ def getMovieData():
         movies_json.append(movie.__dict__)
     
     return movies_json
+
+def getJsonData_links():
     
+    links_relative = "data/links.csv"
+    links_absolute = getAbsolutePathUsing(links_relative)
+    
+    FileReader.sourceFilePath = links_absolute
+    links: [[str]] = FileReader.getRows()
+    
+    links_json = []
+    
+    for row in links:
+        
+        movieID: int = row[0]
+        imdbID: str = row[1]
+        tmdbID: str = row[2]
+        
+        link = Link(movieID, imdbID, tmdbID)
+        links_json.append(link.__dict__)
+    
+    return links_json
 
+def getJsonData_ratings():
+    
+    ratings_relative = "data/ratings.csv"
+    ratings_absolute = getAbsolutePathUsing(ratings_relative)
+    
+    FileReader.sourceFilePath = ratings_absolute
+    ratings: [[str]] = FileReader.getRows()
+    
+    ratings_json = []
+    
+    for row in ratings:
+        
+        userID: int = row[0]
+        movieID: int = row[1]
+        rating: int = row[2]
+        timestamp: str = row[3]
+        
+        rating = Rating(userID, movieID, rating, timestamp)
+        ratings_json.append(rating.__dict__)
+    
+    return ratings_json
 
+def getJsonData_tags():
+    
+    tags_relative = "data/tags.csv"
+    tags_absolute = getAbsolutePathUsing(tags_relative)
+    
+    FileReader.sourceFilePath = tags_absolute
+    tags: [[str]] = FileReader.getRows()
+    
+    tags_json = []
+    
+    for row in tags:
+        
+        userID: int = row[0]
+        movieID: int = row[1]
+        tag: str = row[2]
+        timestamp: str = row[3]
+        
+        tag = Tag(userID, movieID, tag, timestamp)
+        tags_json.append(tag.__dict__)
+    
+    return tags_json
